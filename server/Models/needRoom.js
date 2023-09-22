df0bf211ab2f9271f947b4844378807941b7fd33
Subhodeep Dey <90602098+subhodeep-dey@users.noreply.github.com>
2023-09-23 00:42:30 +0530

    Update needRoom.js

--- server/Models/needRoom.js
+++ server/Models/needRoom.js
@@ -17,10 +17,7 @@ const postSchema = mongoose.Schema(
       type: Number,
       required: true
     },
-    year : {
-      type: Number,
-      required: false
-    },
+    year: String,
     habits: String,
     desc: String,
   },
