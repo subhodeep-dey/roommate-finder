af37c1aa01de4155f299d792e4ad27a7e8fade9c
Subhodeep Dey <90602098+subhodeep-dey@users.noreply.github.com>
2023-09-23 03:19:02 +0530

    Update userModel.js

--- server/Models/userModel.js
+++ server/Models/userModel.js
@@ -18,6 +18,10 @@ const UserSchema = mongoose.Schema(
             type: String,
             required: false
         },
+        regnum: {
+            type: String,
+            required: false
+        },
         gender: String,
         rank : {
             type: Number,
