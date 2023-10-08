0b4eb014b92d89cb54f6f644c703d3f4dc145258
Subhodeep Dey <sdeysocial@gmail.com>
2023-10-08 05:30:18 +0530

    Modified createMailTransporter function
    modified createMailTransporter function to use async/await and promises to handle nodemailer connections and sending emails.

--- server/utils/createMailTransporter.js
+++ server/utils/createMailTransporter.js
@@ -1,13 +1,30 @@
 import nodemailer from 'nodemailer';
 
-const createMailTransporter = () => {
+const createMailTransporter = async () => {
     const transporter = nodemailer.createTransport({
         service: "hotmail",
         auth: {
             user: "cdac-kolkata@outlook.com",
             pass: process.env.EMAIL_PASS,
         },
+        port: 465,
+        host: "smtp.office365.com",
+        secure: true, // true for 465, false for other ports
     });
+
+    // Verify connection configuration
+    await new Promise((resolve, reject) => {
+        transporter.verify((error, success) => {
+            if (error) {
+                console.error(error);
+                reject(error);
+            } else {
+                console.log("Server is ready to take our messages");
+                resolve(success);
+            }
+        });
+    });
+
     return transporter;
 };
 
