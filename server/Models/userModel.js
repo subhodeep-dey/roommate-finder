036674dbaa817e7a8c2854e5fd51520d6b37ba78
Subhodeep Dey <90602098+subhodeep-dey@users.noreply.github.com>
2023-10-30 19:45:14 +0530

    Update userModel.js

--- server/Models/userModel.js
+++ server/Models/userModel.js
@@ -12,31 +12,25 @@ const UserSchema = mongoose.Schema(
     },
     firstname: {
       type: String,
-      default: null  // Set default value to null
     },
     lastname: {
       type: String,
-      default: null  // Set default value to null
     },
     regnum: {
       type: String,
-      default: null  // Set default value to null
     },
     gender: {
       type: String,
-      default: null  // Set default value to null
+      default: null
     },
     rank: {
       type: Number,
-      default: null  // Set default value to null
     },
     year: {
       type: Number,
-      default: null  // Set default value to null
     },
     mobile: {
       type: Number,
-      default: null  // Set default value to null
     },
     isAdmin: {
       type: Boolean,
@@ -44,15 +38,12 @@ const UserSchema = mongoose.Schema(
     },
     profilePicture: {
       type: String,
-      default: null  // Set default value to null
     },
     coverPicture: {
       type: String,
-      default: null  // Set default value to null
     },
     about: {
       type: String,
-      default: null  // Set default value to null
     },
     isVerified: {
       type: Boolean,
@@ -60,23 +51,22 @@ const UserSchema = mongoose.Schema(
     },
     emailToken: {
       type: String,
-      default: null  // Set default value to null
     },
     followers: {
       type: Array,
-      default: []  // Set default value to an empty array
+      default: []
     },
     following: {
       type: Array,
-      default: []  // Set default value to an empty array
+      default: [] 
     },
     likesRoommate: {
       type: Array,
-      default: []  // Set default value to an empty array
+      default: [] 
     },
     likesRoom: {
       type: Array,
-      default: []  // Set default value to an empty array
+      default: []
     }
   },
   { timestamps: true }
